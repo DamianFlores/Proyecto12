b156eff0b1a47d9c46356c38f6a5ee15c4fa8473
DFloresRF-PC\DFloresRF <damian.flores.rf@gmail.com>
2018-12-30 20:08:32 -0300

    Agregando tiempo de rodaje al dado.

--- Proyecto12/Assets/Scripts/Actividades/Tablero/Dado.cs
+++ Proyecto12/Assets/Scripts/Actividades/Tablero/Dado.cs
@@ -7,14 +7,46 @@ public class Dado : MonoBehaviour {
 
     public int valor;
     public int lados = 6;
+    public float incrementoTiempo;
+    public float tiempoMaximo;
     public Tablero tablero;
 
     public Text texto;
 
     public void Lanzar()
     {
-        valor = Random.Range(0, lados) + 1;
-        texto.text = valor.ToString();
+        StartCoroutine(Rodar());
+    }
+
+    private IEnumerator Rodar()
+    {
+        GetComponent<Button>().interactable = false;
+        var tiempo = 0f;
+        var duracion = 0f;
+
+        while(tiempo < tiempoMaximo)
+        {
+            valor = ObtenerNumeroAdyacente(valor);
+            texto.text = valor.ToString();
+
+            duracion += incrementoTiempo;
+            tiempo += duracion;
+
+            yield return new WaitForSeconds(duracion);
+        }
+
         tablero.ResultadoDado(valor);
+        GetComponent<Button>().interactable = true;
+    }
+
+    private int ObtenerNumeroAdyacente(int numero)
+    {
+        int r;
+        do
+        {
+            r = Random.Range(0, lados) + 1;
+        }
+        while (r == numero || r == 7 - numero);
+        return r;
     }
 }
