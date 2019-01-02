5d1ea3dac2578dfdffbfea13cbcfd6bde450b3f9
DFloresRF-PC\DFloresRF <damian.flores.rf@gmail.com>
2019-01-02 04:49:39 -0300

    Cambiando sistema de eventos de casillero

--- Proyecto12/Assets/Scripts/Actividades/Tablero/Casillero.cs
+++ Proyecto12/Assets/Scripts/Actividades/Tablero/Casillero.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Casillero : MonoBehaviour {
 
     public int indice;
-    public UnityEvent accion;
-
+    public System.Action accion;
+    
     public void Accionar()
     {
-        accion.Invoke();
+        accion();
     }
 }
