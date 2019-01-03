5574862c81d431ed9572dbb9ab6535f4f02132af
DFloresRF-PC\DFloresRF <damian.flores.rf@gmail.com>
2019-01-02 23:25:49 -0300

    SO de tema

---
+++ Proyecto12/Assets/Scripts/Actividades/Tablero/Palabras/SO_Tema.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Tema", menuName = "Datos/Tema", order = 1)]
+public class SO_Tema : ScriptableObject
+{
+    public List<Palabra2> palabras;
+}
